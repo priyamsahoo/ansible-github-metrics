7e6ad932020cf51122ac6946dad4b8042f4b636a
Priyam Sahoo <prsahoo@localhost.localdomain>
2021-01-22 22:46:08 +0530

    Added pagination in issue table

--- src/components/IssueTable.js
+++ src/components/IssueTable.js
@@ -1,9 +1,10 @@
 import { useState, useMemo } from "react";
 import { Link } from "react-router-dom";
 import { Table } from 'reactstrap';
-import { useTable, useFilters, useSortBy } from 'react-table';
+import { useTable, useFilters, useSortBy, usePagination } from 'react-table';
 import { COLUMNS } from './IssueColumns';
 import { Filter, DefaultColumnFilter } from './filters';
+import { Button } from "bootstrap";
 
 
 const IssueTable = ({ name, issues, count }) => {
@@ -20,16 +21,28 @@ const IssueTable = ({ name, issues, count }) => {
         defaultColumn: { Filter: DefaultColumnFilter },
         },
         useFilters,
-        useSortBy,);
+        useSortBy,
+        usePagination);
 
     const { 
         getTableProps,
         getTableBodyProps,
         headerGroups,
-        footerGroups,
         rows,
+        page,
+        nextPage,
+        previousPage,
+        canNextPage,
+        canPreviousPage,
+        pageOptions,
+        gotoPage,
+        pageCount,
+        setPageSize,
+        state,
         prepareRow
     } = tableInstance;
+
+    const { pageIndex, pageSize } = state;
 
     const generateSortingIndicator = (column) => {
         return column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : '';
@@ -66,7 +79,7 @@ const IssueTable = ({ name, issues, count }) => {
                 </thead>
                 <tbody { ...getTableBodyProps() }>
                     {
-                        rows.map((row) => {
+                        page.map((row) => {
                             prepareRow(row)
                             return (
                                 <tr { ...row.getRowProps() }>
@@ -87,8 +100,43 @@ const IssueTable = ({ name, issues, count }) => {
                     }
                 </tbody>
             </Table>
+            <div>
+                <span>
+                    Page{ ' ' }
+                    <strong>
+                        { pageIndex + 1 } of { pageOptions.length }
+                    </strong>
+                    { ' ' }
+                </span>
+                <span>
+                    | Go to page: { ' ' }
+                    <input type="number" defaultValue={ pageIndex + 1 }
+                        onChange={ (e) => {
+                            const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(pageNumber)
+                        } }
+                        style={ { width: '50px' } }>
+                    </input>
+                </span>
+                <select value={ pageSize } onChange={ (e) => setPageSize(Number(e.target.value)) }>
+                    {
+                        [10, 25, 50].map((pageSize) => (
+                            <option key={ pageSize } value={ pageSize }>
+                                Show { pageSize }
+                            </option>
+                        ))
+                    }
+                </select>
+                <button onClick={ () => gotoPage(0) } disabled={ !canPreviousPage }>
+                    { '<<' }
+                </button>
+                <button onClick={ () => previousPage() } disabled={ !canPreviousPage }>Previous</button>
+                <button onClick={ () => nextPage() } disabled={ !canNextPage }>Next</button>
+                <button onClick={ () => gotoPage(pageCount - 1) } disabled={ !canNextPage }>
+                    { '>>' }
+                </button>
+            </div>
         </div>
-
     )
 }
  
