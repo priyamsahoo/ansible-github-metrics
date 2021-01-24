6cf9217dd7184024bdb3990a590a8044f87cdc00
agnibha-chatterjee <agni.bhaaa@gmail.com>
2021-01-24 12:56:36 +0530

    both tables can now be filtered using the created at column

--- src/index.js
+++ src/index.js
@@ -2,26 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
-
-
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    HttpLink,
+} from '@apollo/client';
 
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: `${ process.env.REACT_APP_GITHUB_API_URL }`,
-    headers: {
-      Authorization: `bearer ${ process.env.REACT_APP_GITHUB_TOKEN }`
-    }
-  }),
-  cache: new InMemoryCache()
+    link: new HttpLink({
+        uri: 'https://api.github.com/graphql',
+        headers: {
+            Authorization: `bearer 2d7e0c081fa5d44743afde3aefd46d8ac5187bab`,
+        },
+    }),
+    cache: new InMemoryCache(),
 });
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <ApolloProvider client={ client }>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
