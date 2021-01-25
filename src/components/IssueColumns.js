84ef1a031b73b38a94206efc1e46fa35ad38e0c0
Priyam Sahoo <priyamsahoo.26@gmail.com>
2021-01-25 16:19:14 +0530

    Changed the size of the tables

--- src/components/IssueColumns.js
+++ src/components/IssueColumns.js
@@ -1,67 +1,67 @@
-import { format } from 'date-fns';
-import { CustomInput } from 'reactstrap';
-import { DateFilter, SelectColumnFilter } from './filters';
-import moment from 'moment';
-import { CenteredHeader } from './CenteredHeader';
+import { format } from "date-fns";
+import { CustomInput } from "reactstrap";
+import { DateFilter, SelectColumnFilter } from "./filters";
+import moment from "moment";
+import { CenteredHeader } from "./CenteredHeader";
 
 export const COLUMNS = [
-    {
-        Header: <CenteredHeader name='Created At' />,
-        Footer: 'Created At',
-        accessor: 'node.createdAt',
-        Cell: ({ value }) => {
-            return moment(new Date(value)).format('ll');
-        },
-        disableSortBy: true,
-        Filter: DateFilter,
-        filter: (rows, columnId, filterValues) => {
-            return filterValues;
-        },
-        maxWidth: 100,
+  {
+    Header: <CenteredHeader name="Created At" />,
+    Footer: "Created At",
+    accessor: "node.createdAt",
+    Cell: ({ value }) => {
+      return moment(new Date(value)).format("ll");
     },
-    // {
-    //     id: 'node.closed',
-    //     Header: 'Status',
-    //     Footer: 'Status',
-    //     accessor: d => { return d.node.closed ? "Closed" : "Open" },
-    //     Filter: SelectColumnFilter,
-    //     filter: 'equals'
-    // },
-    {
-        Header: <CenteredHeader name='Status' />,
-        Footer: 'Status',
-        accessor: 'node.state',
-        Filter: SelectColumnFilter,
-        filter: 'equals',
-        disableSortBy: true,
-        maxWidth: 100,
+    disableSortBy: true,
+    Filter: DateFilter,
+    filter: (rows, columnId, filterValues) => {
+      return filterValues;
     },
-    {
-        Header: <CenteredHeader name='Title' />,
-        Footer: 'Title',
-        accessor: 'node.title',
-        Cell: ({ cell: { value }, row: { original } }) => (
-            <a href={original.node.url} target='_blank'>
-                {value}
-            </a>
-        ),
-        minWidth: 250,
+    maxWidth: 100,
+  },
+  // {
+  //     id: 'node.closed',
+  //     Header: 'Status',
+  //     Footer: 'Status',
+  //     accessor: d => { return d.node.closed ? "Closed" : "Open" },
+  //     Filter: SelectColumnFilter,
+  //     filter: 'equals'
+  // },
+  {
+    Header: <CenteredHeader name="Status" />,
+    Footer: "Status",
+    accessor: "node.state",
+    Filter: SelectColumnFilter,
+    filter: "equals",
+    disableSortBy: true,
+    maxWidth: 100,
+  },
+  {
+    Header: <CenteredHeader name="Title" />,
+    Footer: "Title",
+    accessor: "node.title",
+    Cell: ({ cell: { value }, row: { original } }) => (
+      <a href={original.node.url} target="_blank">
+        {value}
+      </a>
+    ),
+    minWidth: 250,
+  },
+  {
+    Header: <CenteredHeader name="Updated At" />,
+    Footer: "Updated At",
+    accessor: "node.updatedAt",
+    Cell: ({ value }) => {
+      return moment(new Date(value)).format("ll");
     },
-    {
-        Header: <CenteredHeader name='Updated At' />,
-        Footer: 'Updated At',
-        accessor: 'node.updatedAt',
-        Cell: ({ value }) => {
-            return moment(new Date(value)).format('ll');
-        },
-        disableFilters: true,
-        maxWidth: 100,
-    },
-    {
-        Header: <CenteredHeader name='Author' />,
-        Footer: 'Author',
-        accessor: 'node.author.login',
-        disableFilters: true,
-        maxWidth: 100,
-    },
+    disableFilters: true,
+    maxWidth: 100,
+  },
+  {
+    Header: <CenteredHeader name="Author" />,
+    Footer: "Author",
+    accessor: "node.author.login",
+    disableFilters: true,
+    maxWidth: 100,
+  },
 ];
