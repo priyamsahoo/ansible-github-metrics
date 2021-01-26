1bfa090846e71fb04131e7cc3e40fbdb09fb0ee6
Priyam Sahoo <priyamsahoo.26@gmail.com>
2021-01-26 18:10:03 +0530

    Added ansible logo in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <h1>Ansible Github Metrics</h1>
-            <div className="links">
-                <Link to="/">Collections</Link>
-                <Link to="/developers">Developers</Link>
-            </div>
-        </nav>
-    );
-}
- 
+  return (
+    <nav className="navbar">
+      <img src={logo} width="50" height="50"></img>
+      <h1>nsible Github Metrics</h1>
+      <div className="links">
+        <Link to="/">Collections</Link>
+        <Link to="/developers">Developers</Link>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
