2b2add028d7764e7b11d838e0c4d82d40d9a1b56
Priyam Sahoo <priyamsahoo.26@gmail.com>
2021-01-28 23:10:48 +0530

    Displayed repository-wise issue details per developer

--- src/components/Developers/RepositoryWiseDetails.js
+++ src/components/Developers/RepositoryWiseDetails.js
@@ -1,7 +1,148 @@
-const RepositoryWiseDetails = () => {
+const RepositoryWiseDetails = ({ issueData }) => {
+  console.log(issueData);
+
+  const displayDetails = () => {
+    return (
+      <div className="details-inner">
+        {/* 1. cisco.nxos */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.CISCO_NXOS_OPEN_ISSUES.name}</h2>
+          <p>
+            Issues opened: {issueData.CISCO_NXOS_OPEN_ISSUES.issues.totalCount}
+          </p>
+          <p>
+            Issues closed:{" "}
+            {issueData.CISCO_NXOS_CLOSED_ISSUES.issues.totalCount}
+          </p>
+        </div>
+
+        {/* 2. cisco.ios */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.CISCO_IOS_OPEN_ISSUES.name}</h2>
+          <p>
+            Issues opened: {issueData.CISCO_IOS_OPEN_ISSUES.issues.totalCount}
+          </p>
+          <p>
+            Issues closed: {issueData.CISCO_IOS_CLOSED_ISSUES.issues.totalCount}
+          </p>
+        </div>
+
+        {/* 3. cisco.iosxr */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.CISCO_IOSXR_OPEN_ISSUES.name}</h2>
+          <p>
+            Issues opened: {issueData.CISCO_IOSXR_OPEN_ISSUES.issues.totalCount}
+          </p>
+          <p>
+            Issues closed:{" "}
+            {issueData.CISCO_IOSXR_CLOSED_ISSUES.issues.totalCount}
+          </p>
+        </div>
+
+        {/* 4. arista.eos */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.ARISTA_EOS_OPEN_ISSUES.name}</h2>
+          <p>
+            Issues opened: {issueData.ARISTA_EOS_OPEN_ISSUES.issues.totalCount}
+          </p>
+          <p>
+            Issues closed:{" "}
+            {issueData.ARISTA_EOS_CLOSED_ISSUES.issues.totalCount}
+          </p>
+        </div>
+
+        {/* 5. vyos.vyos */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.VYOS_OPEN_ISSUES.name}</h2>
+          <p>Issues opened: {issueData.VYOS_OPEN_ISSUES.issues.totalCount}</p>
+          <p>Issues closed: {issueData.VYOS_CLOSED_ISSUES.issues.totalCount}</p>
+        </div>
+
+        {/* 6. junipernetworks.junos */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.JUNOS_OPEN_ISSUES.name}</h2>
+          <p>Issues opened: {issueData.JUNOS_OPEN_ISSUES.issues.totalCount}</p>
+          <p>
+            Issues closed: {issueData.JUNOS_CLOSED_ISSUES.issues.totalCount}
+          </p>
+        </div>
+
+        {/* 7. cisco.asa */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.CISCO_ASA_OPEN_ISSUES.name}</h2>
+          <p>
+            Issues opened: {issueData.CISCO_ASA_OPEN_ISSUES.issues.totalCount}
+          </p>
+          <p>
+            Issues closed: {issueData.CISCO_ASA_CLOSED_ISSUES.issues.totalCount}
+          </p>
+        </div>
+
+        {/* 8. ansible.netcommon */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.ANSIBLE_NETCOMMON_OPEN_ISSUES.name}</h2>
+          <p>
+            Issues opened:{" "}
+            {issueData.ANSIBLE_NETCOMMON_OPEN_ISSUES.issues.totalCount}
+          </p>
+          <p>
+            Issues closed:{" "}
+            {issueData.ANSIBLE_NETCOMMON_CLOSED_ISSUES.issues.totalCount}
+          </p>
+        </div>
+
+        {/* 9. frr.frr */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.FRR_OPEN_ISSUES.name}</h2>
+          <p>Issues opened: {issueData.FRR_OPEN_ISSUES.issues.totalCount}</p>
+          <p>Issues closed: {issueData.FRR_CLOSED_ISSUES.issues.totalCount}</p>
+        </div>
+
+        {/* 10. openvswitch.openvswitch */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.OPENVSWITCH_OPEN_ISSUES.name}</h2>
+          <p>
+            Issues opened: {issueData.OPENVSWITCH_OPEN_ISSUES.issues.totalCount}
+          </p>
+          <p>
+            Issues closed:{" "}
+            {issueData.OPENVSWITCH_CLOSED_ISSUES.issues.totalCount}
+          </p>
+        </div>
+
+        {/* 11. community.yang */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.COMMUNITY_YANG_OPEN_ISSUES.name}</h2>
+          <p>
+            Issues opened:{" "}
+            {issueData.COMMUNITY_YANG_OPEN_ISSUES.issues.totalCount}
+          </p>
+          <p>
+            Issues closed:{" "}
+            {issueData.COMMUNITY_YANG_CLOSED_ISSUES.issues.totalCount}
+          </p>
+        </div>
+
+        {/* 12. ansible.utils */}
+        <div className="repository-wise-issue-data">
+          <h2>{issueData.ANSIBLE_UTILS_OPEN_ISSUES.name}</h2>
+          <p>
+            Issues opened:{" "}
+            {issueData.ANSIBLE_UTILS_OPEN_ISSUES.issues.totalCount}
+          </p>
+          <p>
+            Issues closed:{" "}
+            {issueData.ANSIBLE_UTILS_CLOSED_ISSUES.issues.totalCount}
+          </p>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="repository-wise-details">
       <h3>Details</h3>
+      <div className="details">{displayDetails()}</div>
     </div>
   );
 };
--- src/components/Developers/SplitupInfo.js
+++ src/components/Developers/SplitupInfo.js
@@ -1,10 +1,18 @@
+import { useQuery } from "@apollo/client";
+import { DEVELOPER_ISSUE } from "../../queries/developer_issue_query";
 import RepositoryWiseDetails from "./RepositoryWiseDetails";
 
 const SpiltupInfo = ({ selectedDeveloper }) => {
+  const { loading, error, data } = useQuery(DEVELOPER_ISSUE, {
+    variables: { userName: selectedDeveloper },
+  });
+
+  // console.log(data);
+
   return (
     <div className="splitup-info">
       <h2>Repository wise details of {selectedDeveloper}</h2>
-      <RepositoryWiseDetails />
+      {data && <RepositoryWiseDetails issueData={data} />}
     </div>
   );
 };
