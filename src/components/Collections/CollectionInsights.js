05599d965e05947428f4e860c0a2008b920bb375
Priyam Sahoo <priyamsahoo.26@gmail.com>
2021-01-30 00:05:07 +0530

    Added closed issues and merged pr percentage

--- src/components/Collections/CollectionInsights.js
+++ src/components/Collections/CollectionInsights.js
@@ -10,22 +10,58 @@ const CollectionInsights = ({ repository }) => {
 
   // let openIssues = data.repository.issues.totalCount;
 
+  const displayDetails = () => {
+    return (
+      <div className="information">
+        <div className="information-inner">
+          <div className="information-numericals">
+            <p>Open Issues: {data.repository.openIssues.totalCount}</p>
+            <p>Closed Issues: {data.repository.closedIssues.totalCount}</p>
+          </div>
+          <div className="information-percentage">
+            <h3>
+              {(
+                (data.repository.closedIssues.totalCount /
+                  (data.repository.openIssues.totalCount +
+                    data.repository.closedIssues.totalCount)) *
+                100
+              ).toFixed(2)}
+              %
+            </h3>
+            <h4>Closed issues</h4>
+          </div>
+        </div>
+        <br></br>
+        <div className="information-inner">
+          <div className="information-numericals">
+            <p>Open PRs: {data.repository.openPRs.totalCount}</p>
+            <p>Closed PRs: {data.repository.closedPRs.totalCount}</p>
+            <p>Merged PRs: {data.repository.mergedPRs.totalCount}</p>
+          </div>
+          <div className="information-percentage">
+            <h3>
+              {(
+                (data.repository.mergedPRs.totalCount /
+                  (data.repository.openPRs.totalCount +
+                    data.repository.mergedPRs.totalCount)) *
+                100
+              ).toFixed(2)}
+              %
+            </h3>
+            <h4>Merged PRs</h4>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="collection-insights">
       <h2>Collection Insights: {repository}</h2>
       <br></br>
       {error && <div>{error}</div>}
       {loading && <div>Loading...</div>}
-      {data && (
-        <div className="information">
-          <p>Open Issues: {data.repository.openIssues.totalCount}</p>
-          <p>Closed Issues: {data.repository.closedIssues.totalCount}</p>
-          <br></br>
-          <p>Open PRs: {data.repository.openPRs.totalCount}</p>
-          <p>Closed PRs: {data.repository.closedPRs.totalCount}</p>
-          <p>Merged PRs: {data.repository.mergedPRs.totalCount}</p>
-        </div>
-      )}
+      {data && displayDetails()}
     </div>
   );
 };
