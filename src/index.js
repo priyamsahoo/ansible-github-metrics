48c4bbbff57d9af05d7a73531c5ffe6f680307ae
Priyam Sahoo <priyamsahoo.26@gmail.com>
2021-02-21 22:33:08 +0530

    Added apollo client error handling at top level

--- src/index.js
+++ src/index.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
-// import "rsuite/dist/styles/rsuite-default.css";̥
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const httpLink = new HttpLink({
+  // uri: `"${process.env.REACT_APP_GITHUB_URL}"`,
+  uri: `${process.env.REACT_APP_GITHUB_API_URL}`,
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  },
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 const client = new ApolloClient({
-    link: new HttpLink({
-        // uri: `"${process.env.REACT_APP_GITHUB_URL}"`,
-        uri: `${process.env.REACT_APP_GITHUB_API_URL}`,
-        headers: {
-            Authorization: `bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
-        },
-    }),
-    cache: new InMemoryCache(),
+  link: errorLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
-    <React.StrictMode>
-        <ApolloProvider client={client}>
-            <App />
-        </ApolloProvider>
-    </React.StrictMode>,
-    document.getElementById('root'),
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
